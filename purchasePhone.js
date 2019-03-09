6d044877a85f35d3f66b523493ff61ebda9d5e4a
Jing Liu <jing.liu@rea-group.com>
2019-03-09 22:11:01 +1100

    refactor logic into individual functions

--- purchasePhone.js
+++ purchasePhone.js
@@ -1,33 +1,50 @@
-var phonePrice = 100;
-var accesorieyPrice = 10;
-var bankMoney = 5000;
-var thresholdSpending = 4000;
-var taxRate    = 0.1;
-var totalSpendingMoney=0;
-var phoneNumber;
-var total;
-var moneyOfAcc;
-var moneyOfphone;
-
-function caculateTax(totalPrice){
-    var tax = totalPrice*taxRate;
-    return tax;
+function caculateTax(totalPrice, taxRate) {
+    return totalPrice * taxRate;
 }
 
-function totalSpending(){      
-    var n =0;
-    while (((thresholdSpending-totalSpendingMoney)>0)&&((bankMoney-totalSpendingMoney)>0)){
-    n = n+1;
-   totalSpendingMoney = 0;
-    moneyOfAcc = n*accesorieyPrice;
-    moneyOfphone = n*phonePrice;
-    total = moneyOfphone+moneyOfAcc;
-    totalSpendingMoney = total+caculateTax(total);   
+function calculateTotal(n, accesorieyPrice, phonePrice, taxRate) {
+    var moneyOfAcc = n * accesorieyPrice;
+    var moneyOfphone = n * phonePrice;
+    var total = moneyOfphone + moneyOfAcc;
+    var totalSpendingMoney = total + caculateTax(total, taxRate);
+    return totalSpendingMoney;
 }
-   
-    return n;
+
+function isBankrupt(thresholdSpending, totalSpendingMoney, bankMoney) {
+    return ((thresholdSpending - totalSpendingMoney) > 0) && ((bankMoney - totalSpendingMoney) > 0);
 }
-phoneNumber = totalSpending();
-console.log("the number of phone you have bought is:"+phoneNumber+" the money you have spent(with tax) is:$"+totalSpendingMoney);
 
+function buyUntilBankrup(phonePrice, accesorieyPrice, bankMoney, threshold, taxRate) {
+    var total = 0;
+    var n = 0;
+    while (!isBankrupt(threshold, total, bankMoney)) {
+        n = n + 1;
+        total = calculateTotal(n, accesorieyPrice, phonePrice, taxRate);
+    }
+    return {
+        amount: n,
+        totalCost: total,
+    };
+}
 
+function formatPrice(price) {
+    return '$' + price;
+}
+
+function printShopItem(shoppingItem) {
+    var formatedCost = formatPrice(shoppingItem.totalCost);
+    console.log("the number of phone you have bought is:" + shoppingItem.account);
+    console.log("the money you have spent(with tax) is:$" + formatedCost);
+}
+
+function main() {
+    var phonePrice = 100;
+    var accesorieyPrice = 10;
+    var bankMoney = 5000;
+    var thresholdSpending = 4000;
+    var taxRate = 0.1;
+    var shoppingItem = buyUntilBankrup(phonePrice, accesorieyPrice, bankMoney, thresholdSpending, taxRate);
+    printShopItem(shoppingItem);
+}
+
+main();
