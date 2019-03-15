d006f0a63bb46a594718207025de4d6ae47e7d01
Jing Liu <jing.liu@rea-group.com>
2019-03-15 23:30:11 +1100

    delete useless line

--- spec/helpers/reporter.js
+++ spec/helpers/reporter.js
@@ -1,5 +1,4 @@
 const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
-console.log("---");
 
 jasmine.getEnv().clearReporters();               // remove default reporter logs
 jasmine.getEnv().addReporter(new SpecReporter({  // add jasmine-spec-reporter
